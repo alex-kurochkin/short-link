4e2bccb7632aaa568335a3e95bf837ba35f794f4
a.kurochkin <inspector@list.ru>
2026-07-03 23:31:52 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -e
 
+# Установка зависимостей для Laravel 12
+# Однако, неизвестно какая ОС используется
+#sudo apt update
+#sudo apt install -y php8.4 php8.4-cli php8.4-fpm php8.4-common php8.4-sqlite3 php8.4-curl php8.4-mbstring php8.4-xml php8.4-zip php8.4-bcmath php8.4-gd curl unzip git
+# Так же потребуется rsync
+
+# Установка Composer
+#curl -sS https://getcomposer.org/installer | php
+#sudo mv composer.phar /usr/local/bin/composer
+#sudo chmod +x /usr/local/bin/composer
+
 echo "=== 1. Создание проекта Laravel 12 ==="
 
 composer create-project laravel/laravel _temp_laravel "12.*" --quiet --no-interaction
